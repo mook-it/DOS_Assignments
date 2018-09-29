381736e31d1a569e83814be3db5b235f50c4c1e8
Siddhant Mittal <siddhantmittal@Siddhants-MacBook-Pro.local>
2018-09-29 04:50:27 -0400

    adding line topology

--- assign2/code/gvp/lib/gvp/Topologies/topologies.ex
+++ assign2/code/gvp/lib/gvp/Topologies/topologies.ex
@@ -1,15 +1,34 @@
 defmodule Gvp.Topologies do
-  use GenServer
-  def start_link() do
-    GenServer.start_link(__MODULE__, {num_of_nodes, topology}, name: @me)
-  end
-  def init(num_of_nodes, topology, list) do
 
+  def initalise_topology_module(list) do
+    table = :ets.new(:pids_registry, [:set, :protected])
+    :ets.insert(table, {"listOfpids", list})
   end
 
-  def get_neighbour(node_tuple) do
+  def get_neighbour(key,alogrithm) do
+    {_,list} = :ets.lookup(:pids_registry,"listOfpids")
+    get_neighbour_helper(key,list,alogrithm,length(list))
   end
 
-  def update_topology(node_tuple) do
+
+  def get_neighbour_helper(key,list,alogrithm,numNodes) do
+    neighboursList = []
+    cond do
+      alogrithm == "line"  -> neighboursList =  cond do
+        key == 1 -> [key+1]
+        key == numNodes -> [key-1]
+        true -> [key-1,key+1]
+      end
+    end
+    index = :rand.uniform(length(neighboursList))-1
+    neighbour_id =  Enum.at(neighboursList,index)
+    Enum.at(list,neighbour_id)
   end
+
+  def delete_pid(key) do
+    {_,list} = :ets.lookup(:pids_registry,"listOfpids")
+    value = Enum.at(list,key)
+    :ets.update_element(:pids_registry,"listOfpids",List.delete(list,value))
+  end
+
 end
