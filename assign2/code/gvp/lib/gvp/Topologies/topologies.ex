dccf868c7ad7a98935ca2945c1832f4f0374c836
Siddhant Mittal <siddhantmittal@Siddhants-MacBook-Pro.local>
2018-09-29 07:27:03 -0400

    adding new changes

--- assign2/code/gvp/lib/gvp/Topologies/topologies.ex
+++ assign2/code/gvp/lib/gvp/Topologies/topologies.ex
@@ -1,48 +1,28 @@
 defmodule Gvp.Topologies do
 
 
-  def find_indexes(collection, function) do
-    Enum.filter_map(Enum.with_index(collection), fn({x, _y}) -> function.(x) end, elem(&1, 1))
-  end
 
+  def get_neighbours(list,topology) do
 
-  def initalise_topology_module(list,topology) do
-    table = :ets.new(:pids_registry, [:set, :protected])
-    :ets.insert(table, {"listOfpids", list})
-    :ets.insert(table, {"topology", topology})
-  end
+    numNodes = length(list)
 
-  def get_first() do
-    {_,list} = :ets.lookup(:pids_registry,"listOfpids")
-    Enum.at(list,0)
-  end
+    map = %{}
 
-  def get_neighbour(pid) do
-    {_,list} = :ets.lookup(:pids_registry,"listOfpids")
-    {_,topology} = :ets.lookup(:pids_registry,"topology")
-    key = find_indexes(list, fn(x) -> x == pid end)
-    get_neighbour_helper(key,list,topology,length(list))
-  end
-
-
-  def get_neighbour_helper(key,list,topology,numNodes) do
-    neighboursList = []
     cond do
-      topology == "line"  -> neighboursList =  cond do
-        key == 1 -> [key+1]
-        key == numNodes -> [key-1]
-        true -> [key-1,key+1]
-      end
+      topology == "line"  -> for i <- 0..numNodes-1 do
+                               neighboursList =  cond do
+                                 i == 0 -> [i+1]
+                                 i == numNodes-1 -> [i-1]
+                                 true -> [i-1,i+1]
+                               end
+                               l = [] ++ Enum.map(neighboursList, fn(x) ->  Enum.at(list,x)
+                               end)
+                               Map.put(map,Enum.at(list,i),l)
+
+                             end
     end
-    index = :rand.uniform(length(neighboursList))-1
-    neighbour_id =  Enum.at(neighboursList,index)
-    Enum.at(list,neighbour_id)
+
   end
 
-  def delete_pid(key) do
-    {_,list} = :ets.lookup(:pids_registry,"listOfpids")
-    value = Enum.at(list,key)
-    :ets.update_element(:pids_registry,"listOfpids",List.delete(list,value))
-  end
 
 end
