d47d81ca7a1cad36ff93bf0fc45dbc27ddf28a76
Siddhant Mittal <siddhantmittal@Siddhants-MacBook-Pro.local>
2018-09-29 05:02:29 -0400

    adding new changes

--- assign2/code/gvp/lib/gvp/Topologies/topologies.ex
+++ assign2/code/gvp/lib/gvp/Topologies/topologies.ex
@@ -1,20 +1,27 @@
 defmodule Gvp.Topologies do
 
-  def initalise_topology_module(list) do
+  def initalise_topology_module(list,topology) do
     table = :ets.new(:pids_registry, [:set, :protected])
     :ets.insert(table, {"listOfpids", list})
+    :ets.insert(table, {"topology", topology})
   end
 
-  def get_neighbour(key,alogrithm) do
+  def get_first() do
     {_,list} = :ets.lookup(:pids_registry,"listOfpids")
-    get_neighbour_helper(key,list,alogrithm,length(list))
+    Enum.at(list,0)
+  end
+
+  def get_neighbour(key) do
+    {_,list} = :ets.lookup(:pids_registry,"listOfpids")
+    {_,topology} = :ets.lookup(:pids_registry,"topology")
+    get_neighbour_helper(key,list,topology,length(list))
   end
 
 
-  def get_neighbour_helper(key,list,alogrithm,numNodes) do
+  def get_neighbour_helper(key,list,topology,numNodes) do
     neighboursList = []
     cond do
-      alogrithm == "line"  -> neighboursList =  cond do
+      topology == "line"  -> neighboursList =  cond do
         key == 1 -> [key+1]
         key == numNodes -> [key-1]
         true -> [key-1,key+1]
