9f5d4a69a609267cc252b08d80d66810f2e7cb88
prafful13 <prafful.mehrotra@gmail.com>
2018-10-01 17:21:17 -0400

    time funcctionality and changes

--- assign2/gvp/lib/gvp/Gossip/driver.ex
+++ assign2/gvp/lib/gvp/Gossip/driver.ex
@@ -12,29 +12,34 @@ defmodule Gvp.Gossip.Driver do
   end
 
   # SERVER
-  def init({num_of_nodes, topology}) do
+  def init({num_of_nodes, topology, start_time}) do
     Process.send_after(self(), :kickoff, 0)
-    {:ok, {num_of_nodes, topology, []}}
+    {:ok, {num_of_nodes, topology, [], start_time}}
   end
 
-  def handle_info(:kickoff, {node_count, topology, deleted_pids}) do
+  def handle_info(:kickoff, {node_count, topology, deleted_pids, start_time}) do
     1..node_count
     |> Enum.map(fn _ -> Gvp.Gossip.NodeSupervisor.add_node() end)
     |> Gvp.Topologies.initialise(topology)
 
     node = Gvp.Topologies.get_first()
     GenServer.cast(node, :next)
-    {:noreply, {node_count, topology, deleted_pids}}
+    {:noreply, {node_count, topology, deleted_pids, start_time}}
   end
 
-  def handle_cast({:done, pid}, {node_count, topology, deleted_pids}) do
+  def handle_cast({:done, pid}, {node_count, topology, deleted_pids, start_time}) do
     deleted_pids = deleted_pids ++ [pid]
     # IO.inspect deleted_pids
     # IO.puts node_count
     if(node_count <= 1) do
+      IO.puts ""
+      IO.puts "Time taken:"
+      end_time = System.monotonic_time(:millisecond)
+      time_taken = end_time - start_time
+      IO.inspect(time_taken)
       System.halt(0)
     end
 
-    {:noreply, {node_count - 1, topology, deleted_pids}}
+    {:noreply, {node_count - 1, topology, deleted_pids, start_time}}
   end
 end
