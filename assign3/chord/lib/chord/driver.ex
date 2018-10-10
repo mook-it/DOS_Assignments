4c318ee3ec3bf1b085c36b275cb22af73308eb78
prafful13 <prafful.mehrotra@gmail.com>
2018-10-09 22:12:30 -0400

    initial chord implementation

---
+++ assign3/chord/lib/chord/driver.ex
@@ -0,0 +1,83 @@
+defmodule Chord.Driver do
+  use GenServer
+  @me __MODULE__
+
+  # API
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: @me)
+  end
+
+  def get_m() do
+    GenServer.call(@me, :get_m)
+  end
+
+  # SERVER
+  def init({numNodes, numRequests, start_time}) do
+    Process.send_after(self(), :kickoff, 0)
+    {:ok, {numNodes, numRequests, 5, 0, start_time}}
+  end
+
+  def handle_call(:get_m, _from, {numNodes, numRequests, m, ring_length, start_time}) do
+    # IO.puts("in get_m")
+    {:reply, {m}, {numNodes, numRequests, m, ring_length, start_time}}
+  end
+
+  def handle_info(:kickoff, {numNodes, numRequests, m, ring_length, start_time}) do
+    set = MapSet.new()
+    max = :math.pow(2, m) |> round
+    node_set = fill_map(set, numNodes, max)
+    IO.inspect(node_set)
+    {:ok, first_node} = Enum.fetch(node_set, 0)
+    pid = Chord.NodeSupervisor.add_node(first_node)
+    {:ok} = Chord.Node.create_chord_ring(first_node)
+    GenServer.cast(:"node_#{first_node}", {:stabilize})
+    # GenServer.cast(:"node_#{first_node}", {:fix_fingers})
+    ring_length = ring_length + 1
+    # IO.inspect([first_node, pid])
+
+    GenServer.cast(:"node_#{first_node}", {:print_table})
+
+    # Enum.each(
+    #   1..(numNodes - 1),
+    #   fn x ->
+    #     {:ok, node_id} = Enum.fetch(node_set, x)
+    #     pid2 = Chord.NodeSupervisor.add_node(node_id)
+    #     Chord.Node.join_new_node(node_id, first_node)
+    #     GenServer.cast(:"node_#{node_id}", {:fix_fingers})
+    #     ring_length = ring_length + 1
+    #     #IO.inspect(pid2)
+    #     #GenServer.cast(:"node_#{node_id}", {:print_table})
+    #   end
+    # )
+
+    {:ok, node_id} = Enum.fetch(node_set, 1)
+    pid2 = Chord.NodeSupervisor.add_node(node_id)
+    Chord.Node.join_new_node(node_id, first_node)
+    # GenServer.cast(:"node_#{node_id}", {:fix_fingers})
+    ring_length = ring_length + 1
+
+    {:ok, node_id} = Enum.fetch(node_set, 2)
+    pid3 = Chord.NodeSupervisor.add_node(node_id)
+    Chord.Node.join_new_node(node_id, first_node)
+    # GenServer.cast(:"node_#{node_id}", {:fix_fingers})
+    ring_length = ring_length + 1
+
+    Process.sleep(10000)
+    GenServer.cast(:"node_#{first_node}", {:print_table})
+
+    # Enum.each(node_set, fn node_id -> GenServer.call(:"node_#{node_id}", {:print_table}) end)
+
+    # System.halt(0)
+    {:noreply, {numNodes, numRequests, m, ring_length, start_time}}
+  end
+
+  defp fill_map(node_set, numNodes, max) do
+    if(MapSet.size(node_set) >= numNodes) do
+      node_set
+    else
+      rand_node_id = Enum.random(1..max)
+      node_set = MapSet.put(node_set, rand_node_id)
+      fill_map(node_set, numNodes, max)
+    end
+  end
+end
---
+++ assign3/chord/lib/chord/node.ex
@@ -0,0 +1,182 @@
+defmodule Chord.Node do
+  use GenServer, restart: :transient
+
+  # API
+  def start_link(node_id) do
+    # IO.inspect node_id
+    GenServer.start_link(__MODULE__, :no_args, name: :"node_#{node_id}")
+  end
+
+  def create_chord_ring(new_node) do
+    GenServer.call(:"node_#{new_node}", {:create, new_node})
+  end
+
+  def join_new_node(new_node, existing_node) do
+    GenServer.cast(:"node_#{new_node}", {:join, new_node, existing_node})
+  end
+
+  # Server
+  def init(:no_args) do
+    {:ok, {0, 0, %{}}}
+  end
+
+  def handle_call({:create, new_node}, _from, {self_node_id, predecessor, finger_table}) do
+    predecessor = nil
+    self_node_id = new_node
+    finger_table = Map.put(finger_table, 0, self_node_id)
+    {:reply, {:ok}, {self_node_id, predecessor, finger_table}}
+  end
+
+  def handle_cast(
+        {:join, new_node, existing_node},
+        {self_node_id, predecessor, finger_table}
+      ) do
+    IO.puts("in join")
+    predecessor = nil
+    self_node_id = new_node
+    IO.inspect(self_node_id)
+
+    {succ} = GenServer.call(:"node_#{existing_node}", {:find_successor, self_node_id})
+    IO.inspect([self_node_id, "succ", succ])
+    finger_table = Map.put(finger_table, 0, succ)
+
+    GenServer.cast(:"node_#{self_node_id}", {:stabilize})
+    {:noreply, {self_node_id, predecessor, finger_table}}
+  end
+
+  def handle_call({:find_successor, key}, _from, {self_node_id, predecessor, finger_table}) do
+    IO.puts("in find")
+    IO.inspect(finger_table)
+
+    resulting_node =
+      if(key > self_node_id && key <= Map.get(finger_table, 0)) do
+        Map.get(finger_table, 0)
+      else
+        prec_node = closest_preceding_node(key, finger_table, self_node_id)
+
+        if(prec_node == self_node_id) do
+          prec_node
+        else
+          GenServer.call(:"node_#{prec_node}", {:find_successor, key})
+        end
+      end
+
+    {:reply, {resulting_node}, {self_node_id, predecessor, finger_table}}
+  end
+
+  def handle_cast({:stabilize}, {self_node_id, predecessor, finger_table}) do
+    IO.puts("in stabilize")
+    successor = Map.get(finger_table, 0)
+
+    if(successor == self_node_id) do
+    else
+      {x} = GenServer.call(:"node_#{successor}", {:find_predecessor})
+      # IO.inspect([self_node_id, successor])
+
+      successor =
+        if(x > self_node_id && x < successor) do
+          x
+        else
+          successor
+        end
+
+      IO.inspect([self_node_id, "succ", successor])
+
+      Map.get_and_update(finger_table, 0, fn x -> {x, successor} end)
+      GenServer.cast(:"node_#{successor}", {:notify, self_node_id})
+    end
+
+      Process.sleep(100)
+      GenServer.cast(:"node_#{self_node_id}", {:stabilize})
+      {:noreply, {self_node_id, predecessor, finger_table}}
+  end
+
+  def handle_cast({:fix_fingers}, {self_node_id, predecessor, finger_table}) do
+    # IO.puts("in fix_fingers")
+    # m = GenServer.call(Chord.Driver, :get_m)
+    {m} = Chord.Driver.get_m()
+    # IO.inspect(m)
+
+    Enum.each(1..m, fn x ->
+      updated_successor =
+        find_successor_self(self_node_id + :math.pow(2, x - 1), finger_table, self_node_id)
+
+      Map.get_and_update(finger_table, x - 1, fn x -> {x, updated_successor} end)
+    end)
+
+    # IO.inspect(["fingering", finger_table])
+
+    GenServer.cast(:"node_#{self_node_id}", {:fix_fingers})
+    {:noreply, {self_node_id, predecessor, finger_table}}
+  end
+
+  def handle_call({:find_predecessor}, _from, {self_node_id, predecessor, finger_table}) do
+    {:reply, {predecessor}, {self_node_id, predecessor, finger_table}}
+  end
+
+  def handle_cast({:notify, possible_predecessor}, {self_node_id, predecessor, finger_table}) do
+    IO.puts("in notify")
+
+    predecessor =
+      if(
+        predecessor == nil ||
+          (possible_predecessor > predecessor && possible_predecessor < self_node_id) ||
+          predecessor > self_node_id
+      ) do
+        possible_predecessor
+      else
+        predecessor
+      end
+
+    IO.inspect([self_node_id, "poss", possible_predecessor])
+    IO.inspect(predecessor)
+
+    # Map.get_and_update(finger_table, 0, fn x -> {x, possible_predecessor})
+
+    {:noreply, {self_node_id, predecessor, finger_table}}
+  end
+
+  defp find_successor_self(key, finger_table, self_node_id) do
+    # IO.puts("in find self")
+
+    resulting_node =
+      if(key > self_node_id && key <= Map.get(finger_table, 0)) do
+        Map.get(finger_table, 0)
+      else
+        prec_node = closest_preceding_node(key, finger_table, self_node_id)
+
+        if(prec_node == self_node_id) do
+          prec_node
+        else
+          GenServer.call(:"node_#{prec_node}", {:find_successor, key})
+        end
+      end
+
+    resulting_node
+  end
+
+  defp closest_preceding_node(key, finger_table, self_node_id) do
+    keys = Map.keys(finger_table)
+    size_of_table = Enum.count(keys)
+    prec_node = loop(size_of_table, finger_table, key, self_node_id)
+  end
+
+  def handle_cast({:print_table}, {self_node_id, predecessor, finger_table}) do
+    IO.inspect(finger_table)
+    {:noreply, {self_node_id, predecessor, finger_table}}
+  end
+
+  defp loop(size_of_table, finger_table, key, self_node_id) do
+    if(size_of_table == 0) do
+      self_node_id
+    else
+      table_entry = Map.get(finger_table, size_of_table - 1)
+
+      if(table_entry > self_node_id && table_entry < key) do
+        table_entry
+      else
+        loop(size_of_table - 1, finger_table, key, self_node_id)
+      end
+    end
+  end
+end
